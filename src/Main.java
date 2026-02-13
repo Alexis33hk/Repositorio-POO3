377ce6db643212fbd88960da1b74f9e07ba2fb17
Danilo <danilostivenmesaguerra@gmail.com>
2026-02-13 17:16:37 -0500

    Comandos git

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,11 @@ public class Main {
 
         System.out.println("Danilo Steven Mesa Guerra (PYTHON)");
 
-
-
+        System.out.println("1, COMANDOS DE GIT VISTOS");
+        System.out.println("1.git add .");
+        System.out.println("2.GIT COMMIT -M");
+        System.out.println("3.GIT PUSH");
+        System.out.println("4.GIT SWITCH");
 
 
         }
